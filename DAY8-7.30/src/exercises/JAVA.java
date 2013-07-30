ecd68fa9f87359af04a1e13a2d3c70f6874f0011
yeahmy <815834094@qq.com>
2013-07-30 19:38:14 +0800

    7.30

---
+++ DAY8-7.30/src/exercises/JAVA.java
@@ -0,0 +1,10 @@
+package exercises;
+
+/**
+ * Created by Administrator on 13-7-30.
+ */
+public class JAVA {
+    public static void main(String[] args) {
+        System.out.println("!!!");
+    }
+}
